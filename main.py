f1865e494dce39901f53d85fab04a76d2712a352
mylaptop9315-source <mylaptop9315@gmail.com>
2025-11-12 00:45:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import time
+import os
+from flask import Flask
 
-print("✅ Bot is starting...")
+app = Flask(__name__)
 
-while True:
-    print("🤖 Bot is running on Render...")
-    time.sleep(10)
+@app.route('/')
+def home():
+    return "✅ Bot is running on Render!"
+
+if __name__ == '__main__':
+    print("✅ Bot is starting...")
+    while True:
+        print("🤖 Bot is running on Render...")
+        time.sleep(10)
+
+    # नीचे की 2 lines से Render को port मिलेगा
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
